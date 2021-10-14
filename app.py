9f766b5df1bb0bee29afb89f506fc5e44f46ad8e
juancv3d <juancvillarios@hotmail.com>
2021-10-13 21:35:13 -0500

    Fixing the send email form

--- app.py
+++ app.py
@@ -6,8 +6,15 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.urandom(24)
 
-    @app.route('/')
+    @app.route('/', methods=['GET', 'POST'])
     def home():
+        if request.method == 'POST':
+            name = request.form['name']
+            email = request.form['email']
+            phone = request.form['phone']
+            message = request.form['message']
+            # print(name, email, phone, message)
+
         return render_template('index.html')
 
     return app
