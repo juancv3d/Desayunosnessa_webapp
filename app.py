6123cd3b9d238ec85691278eedf02c251973b2d4
juancv3d <juancvillarios@hotmail.com>
2021-10-22 17:09:26 -0500

    Creating a function to manage mails

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 import os
+
+
+email = os.environ.get('EMAIL')
+password = os.environ.get('PASSWORD')
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.urandom(24)
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_USERNAME'] = email
+    app.config['MAIL_PASSWORD'] = password
+    mail = Mail(app)
 
     @app.route('/', methods=['GET', 'POST'])
     def home():
         if request.method == 'POST':
             name = request.form['name']
-            email = request.form['email']
+            email_user = request.form['email']
             phone = request.form['phone']
             message = request.form['message']
-            # print(name, email, phone, message)
+            message_to_send = f"Name: {name}\nEmail: {email_user}\nPhone: {phone}\nMessage: {message}"
+            msg = Message(subject="Contacto desayunosnessa",
+                          sender=email, recipients=[email], body=message_to_send)
+            mail.send(msg)
 
         return render_template('index.html')
 
